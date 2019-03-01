d609e15084fdeedf01dc16e1d98932d9406bc8fe
Milan Miljus <milan.997@hotmail.com>
2019-03-01 13:36:35 +0100

    Verzija 1.0

---
+++ src/main/resources/static/precache-manifest.f44e1978fb11f49278e5271d09f33a4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "67ee30a79981d3a3aba6",
+    "url": "/static/js/main.67ee30a7.chunk.js"
+  },
+  {
+    "revision": "ef8e2dbb3a29db4f7967",
+    "url": "/static/js/1.ef8e2dbb.chunk.js"
+  },
+  {
+    "revision": "67ee30a79981d3a3aba6",
+    "url": "/static/css/main.cdd3f435.chunk.css"
+  },
+  {
+    "revision": "a94a56614ef11eba3bece09ed3683e68",
+    "url": "/index.html"
+  }
+];
